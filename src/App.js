ea547d241b9bd7c9e6216ced79dba76fdaf24d39
Takixu <calebxust@outlook.com>
2024-07-25 20:28:30 +1200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Author/Layout';
+import Dashboard from './Author/Dashboard';  
+import Submission from './Author/Submission';  
+import Conference from './Author/Conference';  
+import Message from './Author/Message';  
+import Profile from './Author/Profile';  
+import Settings from './Author/Settings';
+import Support from './Author/Support';
+import Calendar from './Author/Calendar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="submission" element={<Submission />} />
+          <Route path="conference" element={<Conference />} />
+          <Route path="message" element={<Message />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="support" element={<Support />} />
+          <Route path="calendar" element={<Calendar />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
